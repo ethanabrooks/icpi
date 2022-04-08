bacdfeac2365071431fe60cb38992a1d3d3b1680
Ethan Brooks <ethanabrooks@gmail.com>
2022-04-08 13:38:51 -0400

    reformat prompt

--- gql/__init__.py
+++ gql/__init__.py
@@ -3,9 +3,16 @@ import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+prompt = """\
+You are at state 3. Go left. You are at state 2. Receive a reward.
+You are at state 2. Receive a reward.
+You are at state 0. Go right. You are at state 1. Go right. You are at state 2. Receive a reward.
+You are at state 4. Go left. You are at 3. Go left. You are at state 2. Receive a reward. 
+You are at state 1. Go right. You are at state 2. Receive a reward.\
+"""
 response = openai.Completion.create(
     engine="text-davinci-002",
-    prompt="You are at state 3. Go left. You are at state 2. Receive a reward.\nYou are at state 2. Receive a reward.\nYou are at state 0. Go right. You are at state 1. Go right. You are at state 2. Receive a reward.\nYou are at state 4. Go left. You are at 3. Go left. You are at state 2. Receive a reward. \nYou are at state 1. Go right. You are at state 2. Receive a reward.\n",
+    prompt=prompt,
     temperature=0.1,
     max_tokens=200,
     top_p=1,
