7aadca48375393ccbdff3b8a4d8e34492bb42adc
Ethan Brooks <ethanabrooks@gmail.com>
2022-04-19 23:45:29 -0400

    use same prompt_size for pi and q

--- gqn/train.py
+++ gqn/train.py
@@ -28,8 +28,7 @@ def train(
     max_steps: int,
     max_trajectory: int,
     min_successes: int,
-    q_prompt_size: int,
-    pi_prompt_size: int,
+    prompt_size: int,
     seed: int,
     temperature: float,
     top_p: float,
@@ -66,7 +65,7 @@ def train(
         gamma=gamma,
         gpt3=gpt3,
         max_steps=max_trajectory,
-        prompt_size=pi_prompt_size,
+        prompt_size=prompt_size,
         rng=rng,
         debug=debug,
     )
@@ -78,7 +77,7 @@ def train(
         gamma=gamma,
         gpt3=gpt3,
         max_steps=max_trajectory,
-        prompt_size=q_prompt_size,
+        prompt_size=prompt_size,
         rng=rng,
         debug=debug,
     )
