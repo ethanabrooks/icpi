bf0c592738496128f1cc34028fa025294189ff5c
Ethan Brooks <ethanabrooks@gmail.com>
2022-04-26 14:24:19 -0400

    refactor to use tqdm in collect_trajectories

--- gqn/collect_trajectories.py
+++ gqn/collect_trajectories.py
@@ -3,21 +3,19 @@ import pickle
 from agent.train import make_env
 from dollar_lambda import argument, command
 from envs.base_env import Env, TimeStep
+from tqdm import tqdm
 
 
-def collect_trajectories(env: Env, num_trajectories: int):
-    trajectories = []
-    for _ in range(num_trajectories):
-        trajectory = []
-        state = env.reset()
-        done = False
-        while not done:
-            action = env.action_space.sample()
-            next_state, reward, done, _ = env.step(action)
-            trajectory.append(TimeStep(state, action, reward, next_state, done))
-            state = next_state
-        trajectories.append(trajectory)
-    return trajectories
+def collect_trajectory(env: Env):
+    trajectory = []
+    state = env.reset()
+    done = False
+    while not done:
+        action = env.action_space.sample()
+        next_state, reward, done, _ = env.step(action)
+        trajectory.append(TimeStep(state, action, reward, next_state, done))
+        state = next_state
+    return trajectory
 
 
 @command(parsers=dict(env_id=argument("env_id")))
@@ -28,7 +26,7 @@ def main(
     save_path: str = "logs/trajectories.pkl",
 ):
     env = make_env(env_id, gamma=1.0, seed=seed)
-    trajectories = collect_trajectories(env, num_trajectories)
+    trajectories = [collect_trajectory(env) for _ in tqdm(range(num_trajectories))]
 
     with open(save_path, "wb") as f:
         pickle.dump(trajectories, f)
