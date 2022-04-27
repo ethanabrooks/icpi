50ed7908392f43ff4af3fa377bc6e0f44013cf58
Ethan Brooks <ethanabrooks@gmail.com>
2022-04-26 20:22:49 -0400

    Refactor compare_features to compare different prompt-sizes

--- gqn/collect_trajectories.py
+++ gqn/collect_trajectories.py
@@ -1,29 +1,32 @@
 import pickle
 
 from agent.train import make_env
-from dollar_lambda import command
+from dollar_lambda import argument, command
 from envs.base_env import Env, TimeStep
+from tqdm import tqdm
 
 
-def collect_trajectories(env: Env, num_trajectories: int):
-    trajectories = []
-    for _ in range(num_trajectories):
-        trajectory = []
-        state = env.reset()
-        done = False
-        while not done:
-            action = env.action_space.sample()
-            next_state, reward, done, _ = env.step(action)
-            trajectory.append(TimeStep(state, action, reward, next_state, done))
-            state = next_state
-        trajectories.append(trajectory)
-    return trajectories
+def collect_trajectory(env: Env):
+    trajectory = []
+    state = env.reset()
+    done = False
+    while not done:
+        action = env.action_space.sample()
+        next_state, reward, done, _ = env.step(action)
+        trajectory.append(TimeStep(state, action, reward, next_state, done))
+        state = next_state
+    return trajectory
 
 
-@command()
-def main(env_id: str, seed: int, num_trajectories: int, save_path: str):
+@command(parsers=dict(env_id=argument("env_id")))
+def main(
+    env_id: str,
+    num_trajectories: int,
+    seed: int = 0,
+    save_path: str = "logs/trajectories.pkl",
+):
     env = make_env(env_id, gamma=1.0, seed=seed)
-    trajectories = collect_trajectories(env, num_trajectories)
+    trajectories = [collect_trajectory(env) for _ in tqdm(range(num_trajectories))]
 
     with open(save_path, "wb") as f:
         pickle.dump(trajectories, f)
