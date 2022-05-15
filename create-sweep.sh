c241e1d8b00817a6569359e03c3e7d3f4b4c2adc
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2022-05-15 08:42:25 -0400

    Pass custom name to docker-compose runs

--- create-sweep.sh
+++ create-sweep.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-docker-compose -f docker-compose.yml -f $1 down
-docker-compose -f docker-compose.yml -f $1 build --pull
-docker-compose -f docker-compose.yml -f $1 --env-file .env up --remove-orphans --force-recreate
+docker-compose -p $2 -f docker-compose.yml -f $1 down
+docker-compose -p $2 -f docker-compose.yml -f $1 build --pull
+docker-compose -p $2 -f docker-compose.yml -f $1 --env-file .env up --remove-orphans --force-recreate
