174ca18705c57ad65a2d1f60e82a594b3936b04a
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2022-05-17 14:06:22 -0400

    Remove wait_time

--- gqn/rl/train.py
+++ gqn/rl/train.py
@@ -55,7 +55,6 @@ def train(
     if model_name in OPENAI_MODELS:
         lm = GPT3(**kwargs)
     elif model_name in HF_MODELS:
-        del kwargs["wait_time"]
         kwargs["model_name"] = HF_MODELS[kwargs["model_name"]]
         lm = HuggingFaceModel(seed=seed, **kwargs)
     else:
