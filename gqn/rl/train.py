78dd0d40240be5e8f1bf35d61e970bb919de1449
Ethan Brooks <ethanabrooks@gmail.com>
2022-06-14 21:42:38 -0400

    add --sil

--- gqn/rl/train.py
+++ gqn/rl/train.py
@@ -29,6 +29,7 @@ def train(
     model_name: str,
     require_cache: bool,
     seed: int,
+    sil: bool,
     success_buffer_size: int,
     t_threshold: Optional[int],
     temperature: float,
@@ -164,7 +165,10 @@ def train(
         trajectory = trajectory[-env.max_trajectory() :]
         if not timed_out:
             buffer.append(trajectory)
-            if get_value(*trajectory, gamma=env.gamma()) > env.failure_threshold():
+            if (
+                not sil
+                or get_value(*trajectory, gamma=env.gamma()) > env.failure_threshold()
+            ):
                 success_buffer.append(trajectory)
 
     print("done!")
