16b1f1584760de7858fb3c07eb80b76b95723462
Ethan Brooks <ethanabrooks@gmail.com>
2022-07-29 18:00:58 -0400

    detach from runs in run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 #! /usr/bin/env bash
 name=$(basename "$PWD")_run
 docker build -f Dockerfile -t "$name" .
-docker run --rm -it \
-	--env-file .env \
-	--shm-size=10.24gb \
+docker run --detach --rm -it \
+  --name "$name" \
+  --env-file .env \
+  --shm-size=10.24gb \
   --network="host" \
-	-h="$(hostname -s)" \
-	-e TERM=xterm-256color \
-	-v "$(pwd)/logs:/root/logs" \
-	-v "$(pwd)/completions:/root/completions" \
-	-v "$HUGGINGFACE_CACHE_DIR:/root/.cache/huggingface/" \
-	"$name" "${@:1}"
+  -h="$(hostname -s)" \
+  -e TERM=xterm-256color \
+  -v "$(pwd)/logs:/root/logs" \
+  -v "$(pwd)/completions:/root/completions" \
+  -v "$HUGGINGFACE_CACHE_DIR:/root/.cache/huggingface/" \
+  "$name" "${@:1}"
+docker logs -f "$name"
