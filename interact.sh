6f659939f6ffe2fcb62d6d2550634dd2393da3ba
Ethan Brooks <ethanabrooks@gmail.com>
2022-08-02 11:34:27 -0400

    add interact.sh

---
+++ interact.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+name=$(basename "$PWD")_run
+docker build -f Dockerfile -t "$name" .
+docker run --rm -it \
+	--name "$name" \
+	--env-file .env \
+	--shm-size=10.24gb \
+	--network="host" \
+	-h="$(hostname -s)" \
+	-e TERM=xterm-256color \
+	-v "$(pwd)/logs:/root/logs" \
+	-v "$(pwd)/completions:/root/completions" \
+	-v "$HUGGINGFACE_CACHE_DIR:/root/.cache/huggingface/" \
+	"$name" "${@:1}"
