b88f7797ba44287f2736da88d9014dcd7036a5c8
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2023-08-04 14:56:36 -0400

    fix flake on macos

--- flake.nix
+++ flake.nix
@@ -4,90 +4,93 @@
     utils.url = "github:numtide/flake-utils/93a2b84fc4b70d9e089d029deacc3583435c2ed6";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    utils,
-  }: let
-    out = system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        config = {
-          allowUnfree = true;
-        };
-      };
-      inherit (pkgs) poetry2nix;
-      mujoco = fetchTarball {
-        url = "https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz";
-        sha256 = "sha256:1lvppcdfca460sqnb0ryrach6lv1g9dwcjfim0dl4vmxg2ryaq7p";
-      };
-      overrides = pyfinal: pyprev: rec {
-        mujoco-py =
-          (pyprev.mujoco-py.override {
-            preferWheel = false;
-          })
-          .overridePythonAttrs (old: {
-            env.NIX_CFLAGS_COMPILE = "-L${pkgs.mesa.osmesa}/lib";
-            preBuild = ''
-              export MUJOCO_PY_MUJOCO_PATH="${mujoco}"
-              export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${mujoco}/bin:${pkgs.mesa.osmesa}/lib:${pkgs.libGL}/lib:${pkgs.gcc-unwrapped.lib}/lib
-            '';
-            buildInputs =
-              old.buildInputs
-              ++ [
-                pyfinal.setuptools
-                pkgs.mesa
-                pkgs.libGL
-              ];
-            patches = [./mujoco-py.patch];
-          });
-        bsuite = pyprev.bsuite.overridePythonAttrs (old: {
-          buildInputs = (old.buildInputs or []) ++ [pyprev.scipy];
-        });
-        dollar-lambda = pyprev.dollar-lambda.overridePythonAttrs (old: {
-          buildInputs = (old.buildInputs or []) ++ [pyprev.poetry];
-        });
-        run-logger = pyprev.run-logger.overridePythonAttrs (old: {
-          buildInputs = (old.buildInputs or []) ++ [pyprev.poetry];
-        });
-        sweep-logger = pyprev.sweep-logger.overridePythonAttrs (old: {
-          buildInputs = (old.buildInputs or []) ++ [pyprev.poetry];
-        });
-        torch = pyprev.pytorch-bin.overridePythonAttrs (old: {
-          src = pkgs.fetchurl {
-            url = "https://download.pytorch.org/whl/cu116/torch-1.13.1%2Bcu116-cp39-cp39-linux_x86_64.whl";
-            sha256 = "sha256-20V6gi1zYBO2/+UJBTABvJGL3Xj+aJZ7YF9TmEqa+sU=";
+  outputs =
+    { self
+    , nixpkgs
+    , utils
+    ,
+    }:
+    let
+      out = system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config = {
+              allowUnfree = true;
+            };
           };
-        });
-        vega-charts = pyprev.vega-charts.overridePythonAttrs (old: {
-          buildInputs = (old.buildInputs or []) ++ [pyprev.poetry];
-        });
-        torchrl = pyprev.torchrl.overridePythonAttrs (old: {
-          preFixup = "addAutoPatchelfSearchPath ${pyfinal.torch}";
-        });
-      };
-      poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
-        python = pkgs.python39;
-        projectDir = ./.;
-        preferWheels = true;
-        overrides = poetry2nix.overrides.withDefaults overrides;
-      };
-    in {
-      devShell = pkgs.mkShell {
-        LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.mesa.osmesa}/lib:${pkgs.libGL}/lib:${pkgs.gcc-unwrapped.lib}/lib";
-        buildInputs = with pkgs; [
-          alejandra
-          poetry
-          poetryEnv
-        ];
-        PYTHONBREAKPOINT = "ipdb.set_trace";
-        shellHook = ''
-          set -o allexport
-          source .env
-          set +o allexport
-        '';
-      };
-    };
-  in
+          inherit (pkgs) poetry2nix lib stdenv;
+          mujoco = fetchTarball {
+            url = "https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz";
+            sha256 = "sha256:1lvppcdfca460sqnb0ryrach6lv1g9dwcjfim0dl4vmxg2ryaq7p";
+          };
+          overrides = pyfinal: pyprev: rec {
+            mujoco-py =
+              (pyprev.mujoco-py.override {
+                preferWheel = false;
+              }).overridePythonAttrs (old: {
+                env.NIX_CFLAGS_COMPILE = "-L${pkgs.mesa.osmesa}/lib";
+                preBuild = ''
+                  export MUJOCO_PY_MUJOCO_PATH="${mujoco}"
+                  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${mujoco}/bin:${pkgs.mesa.osmesa}/lib:${pkgs.libGL}/lib:${pkgs.gcc-unwrapped.lib}/lib
+                '';
+                buildInputs =
+                  old.buildInputs
+                  ++ [
+                    pyfinal.setuptools
+                    pkgs.mesa
+                    pkgs.libGL
+                  ];
+                patches = [ ./mujoco-py.patch ];
+              });
+            bsuite = pyprev.bsuite.overridePythonAttrs (old: {
+              buildInputs = (old.buildInputs or [ ]) ++ [ pyprev.scipy ];
+            });
+            dollar-lambda = pyprev.dollar-lambda.overridePythonAttrs (old: {
+              buildInputs = (old.buildInputs or [ ]) ++ [ pyprev.poetry ];
+            });
+            run-logger = pyprev.run-logger.overridePythonAttrs (old: {
+              buildInputs = (old.buildInputs or [ ]) ++ [ pyprev.poetry ];
+            });
+            sweep-logger = pyprev.sweep-logger.overridePythonAttrs (old: {
+              buildInputs = (old.buildInputs or [ ]) ++ [ pyprev.poetry ];
+            });
+            torch = pyprev.pytorch-bin.overridePythonAttrs (old: {
+              src = pkgs.fetchurl {
+                url = "https://download.pytorch.org/whl/cu116/torch-1.13.1%2Bcu116-cp39-cp39-linux_x86_64.whl";
+                sha256 = "sha256-20V6gi1zYBO2/+UJBTABvJGL3Xj+aJZ7YF9TmEqa+sU=";
+              };
+            });
+            vega-charts = pyprev.vega-charts.overridePythonAttrs (old: {
+              buildInputs = (old.buildInputs or [ ]) ++ [ pyprev.poetry ];
+            });
+            torchrl = pyprev.torchrl.overridePythonAttrs (old: {
+              preFixup = "addAutoPatchelfSearchPath ${pyfinal.torch}";
+            });
+          };
+          poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
+            python = pkgs.python39;
+            projectDir = ./.;
+            preferWheels = true;
+            overrides = poetry2nix.overrides.withDefaults overrides;
+          };
+        in
+        {
+          devShell = pkgs.mkShell {
+            LD_LIBRARY_PATH = lib.optional stdenv.isLinux "$LD_LIBRARY_PATH:${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.mesa.osmesa}/lib:${pkgs.libGL}/lib:${pkgs.gcc-unwrapped.lib}/lib";
+            buildInputs = with pkgs; [
+              alejandra
+              poetry
+              poetryEnv
+            ];
+            PYTHONBREAKPOINT = "ipdb.set_trace";
+            shellHook = ''
+              set -o allexport
+              source .env
+              set +o allexport
+            '';
+          };
+        };
+    in
     utils.lib.eachDefaultSystem out;
 }
